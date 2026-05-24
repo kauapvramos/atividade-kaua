a14cc8dd492a48c8165cfd9462ece573395b4993
kauapvramos <kauaramos735@gmail.com>
2026-05-24 09:55:35 -0300

    falta isfull

--- queue.py
+++ queue.py
@@ -12,5 +12,30 @@ class Queue:
     
     def enqueue(self, elemento: Any) -> None:
      self.__data.append(elemento)
+    
+    def peek(self) -> Any:
+      if not self.__data:
+        return None
+      return self.__data[-1]
+    
+
+    def is_emply(self) -> Any:
+     if len(self.__data) == 0:
+       return print("Lista Vazia")
+     else:
+       print("Nao esta Vazia Lista tem: ",len(self.__data), "elementos")
+     return self.__data
+    
+    def tamanho(self) -> Any:
+       print("Tamanho: ",len(self.__data))
+       return len(self.__data)
+    
+
+    def clear(self) -> Any: 
+      self.__data.clear()
+      print("Lista LIMPA")
+
+    
+
 
 
