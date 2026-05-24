9c78320461320cc4e12ed6906a575bb689636df8
kauapvramos <kauaramos735@gmail.com>
2026-05-24 16:24:53 -0300

    Novo commmit

--- queue.py
+++ queue.py
@@ -1,41 +1,63 @@
 from typing import Any,List
 
-
-
 class Queue:
 
-    def __init__(self) -> None:
+    def __init__(self, max_size: int = None) -> None:
      self.__data: List[Any] = []
+     self.__max_size = max_size
 
     def __repr__(self) -> str:
      return str(self.__data)
     
+    def is_full(self) -> bool:
+      if (self.size == self.__max_size ):
+        return True
+      else:
+        return False
+    
     def enqueue(self, elemento: Any) -> None:
-     self.__data.append(elemento)
+        if self.is_full():
+          print("Lista cheia")
+    
+        else:
+         self.__data.append(elemento)
+    
+    def dequeue(self, elemento: Any) -> None:
+        if len(self.__data) == 0:
+         print("Lista Vazia")
+        else:
+         self.__data.pop(elemento[0])
+
+    def is_emply(self) -> bool:
+     if len(self.__data) == 0:
+       return True
+     else:
+       return False     
     
     def peek(self) -> Any:
-      if not self.__data:
-        return None
-      return self.__data[-1]
+       if self.is_emply == True:
+         print("Lista Vazia")
+       return self.__data[0]
+     
     
-
-    def is_emply(self) -> Any:
-     if len(self.__data) == 0:
-       return print("Lista Vazia")
-     else:
-       print("Nao esta Vazia Lista tem: ",len(self.__data), "elementos")
-     return self.__data
-    
-    def tamanho(self) -> Any:
-       print("Tamanho: ",len(self.__data))
-       return len(self.__data)
+    def size(self) -> Any:
+      print("Tamanho da Lista:", len(self.__data))
+      return len(self.__data)
     
 
     def clear(self) -> Any: 
       self.__data.clear()
       print("Lista LIMPA")
 
-    
 
+if __name__ == "__main__":
+   lista = Queue(3)
+   lista.enqueue(5)
+   lista.enqueue(3)
+   print(lista)
+   lista.dequeue
+   lista.peek
+   print(lista.is_emply())
+   print(lista.size())
 
-
+   print(lista)
